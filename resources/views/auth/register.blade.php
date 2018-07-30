14c90c6782e77aa14abd63edcf4a3e2cbbd7ebc2
linlumix <linlumix@gmail.com>
2018-07-30 19:15:35 -0300

    add js validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,7 +34,7 @@
                 </div>
 
                 <div class="input-group input-group-icon">
-                    <input type="password"  name="contrasenia" placeholder="Contraseña (mayúscula y número requerido)" />
+                    <input type="password"  name="contraseña" placeholder="Contraseña (mayúscula y número requerido)" />
                     <div class="input-icon">
                         <i class="fas fa-lock"></i>
                     </div>
@@ -42,7 +42,7 @@
                 </div>
 
                 <div class="input-group input-group-icon">
-                    <input type="password"  name="contrasenia_confirmation" placeholder="Repite la contraseña" />
+                    <input type="password"  name="contraseña_confirmation" placeholder="Repite la contraseña" />
                     <div class="input-icon">
                         <i class="fas fa-lock"></i>
                     </div>
@@ -67,68 +67,6 @@
         </div>
     </main>
 
-<script type="text/javascript">
-var formreg = document.getElementById('reg-form');
-var error = document.querySelector('.error');
-var inputName = formreg.elements.nombre;
-var inputEmail = formreg.elements.email;
-var inputPass = formreg.elements.contrasenia;
-var inputPass2 = formreg.elements.contrasenia_confirmation;
+<script type="text/javascript" src="{{ URL::asset('js/validacion.js') }}"></script>
 
-inputName.onfocus = function() {
-  error.innerText = '';
-};
-
-inputName.onblur = function() {
-  if(this.value == '') {
-    error.innerText = 'Por favor escribir tu nombre de usuario';
-  }
-};
-
-inputEmail.onfocus = function() {
-  error.innerText = '';
-};
-
-inputEmail.onblur = function() {
-  if(this.value == '') {
-    error.innerText = 'Por favor escribir tu email';
-  }
-};
-
-inputPass.onfocus = function() {
-  error.innerText = '';
-};
-
-inputPass.onblur = function() {
-  if(this.value == '') {
-    error.innerText = 'Por favor escribir tu contraseña';
-  }
-};
-
-inputPass2.onfocus = function() {
-  error.innerText = '';
-};
-
-inputPass2.onblur = function() {
-  if(this.value == '') {
-    error.innerText = 'Por favor escribir tu contraseña';
-  }
-};
-
-formreg.addEventListener('submit', function(e){
-e.preventDefault();
-
-if(inputName.value == '') {
-error.innerText = 'Sin nombre no podés registrarte';
-} else if(inputEmail.value == '') {
-  error.innerText = 'Sin email no podés registrarte';
-} else if(inputPass.value == '') {
-  error.innerText = 'Sin contraseña no podés registrarte';
-} else if(inputPass2.value == '') {
-  error.innerText = 'Sin confirmación de contraseña no podés registrarte';
-} else {
-  this.submit();
-}
-});
-</script>
 @endsection
