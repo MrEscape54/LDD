42444769d4df4d3fffacff10b8c940ee95eb93ca
linlumix <linlumix@gmail.com>
2018-07-30 20:05:29 -0300

    image link fixed

--- resources/views/products/watches.blade.php
+++ resources/views/products/watches.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
 <div class="destacados">
-   @foreach ($products as $product) 
+   @foreach ($products as $product)
 
       <div class="collections coll-watches">
-         <img class="foto" src="{{$product->picture}}">
-            <p class="prod-description">{{$product->description}}</p>
+         <img class="foto" src="{{ asset($product->picture) }}">
+            <p class="prod-description">{{ $product->description }}</p>
             <p class="prod-description prices"><strong>$ {{ number_format($product->price, 0, ",", ".") }}</strong></p>
             <a class="boton-tr boton-tr-watches" href="#">Agregar al carrito</a>
       </div>
