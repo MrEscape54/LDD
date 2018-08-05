642752ef5f1f08951c0f4e0cbaeadec5cdd61be0
linlumix <linlumix@gmail.com>
2018-08-05 15:07:18 -0300

    styleswitcher test1 files

---
+++ public/js/styleswitcher.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  document.getElementById('styleicon').onclick = function() {
+    document.getElementById('style_css').href='/css/style2.css';
+    document.getElementById('contact_css').href='/css/contact2.css';
+  };
+};
